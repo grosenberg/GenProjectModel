98c5b8948050687aa5a8cc07dc362fa187b0a56e
GRosenberg <gbrosenberg85@gmail.com>
2014-12-06 15:10:00 -0800

    Cleanup

--- src/net/certiv/json/converter/Converter.java
+++ src/net/certiv/json/converter/Converter.java
@@ -29,16 +29,18 @@ import net.certiv.json.parser.JsonErrorListener;
 import net.certiv.json.parser.JsonTokenFactory;
 import net.certiv.json.parser.gen.JsonLexer;
 import net.certiv.json.parser.gen.JsonParser;
+import net.certiv.json.parser.gen.JsonParser.JsonContext;
 import net.certiv.json.util.Log;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Converter {
 
 	private IOProcessor processor;
+	private PhaseState state;
+	private String lastError = "<none>";
 
 	public Converter(IOProcessor processor) {
 		super();
@@ -50,56 +52,66 @@ public class Converter {
 	}
 
 	public String convert(String srcData, PhaseState state) {
-		String lastError = "<none>";
+		this.state = state;
 		try {
-			lastError = "Failure in acquiring input stream.";
-			ByteArrayInputStream is = new ByteArrayInputStream(srcData.getBytes());
-			ANTLRInputStream input = new ANTLRInputStream(is);
-
-			lastError = "Failure in generating lexer token stream.";
-			JsonLexer lexer = new JsonLexer(input);
-			// lexer.setLexerHelper(new LexerHelper());
-			JsonTokenFactory factory = new JsonTokenFactory(input);
-			lexer.setTokenFactory(factory);
-			CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-			lastError = "Failure in parser production.";
-			JsonParser parser = new JsonParser(tokens);
-			parser.setTokenFactory(factory);
-			parser.removeErrorListeners(); // remove ConsoleErrorListener
-			parser.addErrorListener(new JsonErrorListener());
-			// parser.setErrorHandler(new JsonParserErrorStrategy());
-			ParseTree tree = parser.json();
-
+			JsonContext tree = parse(srcData);
 			ParseTreeWalker walker = new ParseTreeWalker();
-
-			lastError = "Failure in parse phase 1.";
-			state.tokens = tokens;
-			JsonPhase01 phase01 = new JsonPhase01(state, processor);
-			phase01.collectComments(true);
-			walker.walk(phase01, tree);
-
-			lastError = "Failure in parse phase 2.";
-			JsonPhase02 phase02 = new JsonPhase02(phase01, processor);
-			walker.walk(phase02, tree);
-
-			if (!JsonPhase02.statusResolved) {
-				Log.warn(this, "Failure to resolve source description");
-				return "";
-			}
-
-			lastError = "Failure in parse phase 3.";
-			JsonPhase03 phase03 = new JsonPhase03(phase02, processor);
-			walker.walk(phase03, tree);
-			Log.info(this, "Convertion complete.");
-
+			JsonPhase01 phase01 = processPhase01(tree, walker);
+			JsonPhase02 phase02 = processPhase02(tree, walker, phase01);
+			/* JsonPhase03 phase03 = */processPhase03(tree, walker, phase02);
 			Log.info(this, "Convertion complete.");
 			return state.doc.toString();
-
 		} catch (IOException e) {
 			Log.error(this, lastError, e);
 			return "";
 		}
+	}
+
+	private JsonContext parse(String srcData) throws IOException {
+		lastError = "Failure in acquiring input stream.";
+		ByteArrayInputStream is = new ByteArrayInputStream(srcData.getBytes());
+		ANTLRInputStream input = new ANTLRInputStream(is);
+
+		lastError = "Failure in generating lexer token stream.";
+		JsonLexer lexer = new JsonLexer(input);
+		JsonTokenFactory factory = new JsonTokenFactory(input);
+		lexer.setTokenFactory(factory);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		state.tokens = tokens;
+
+		lastError = "Failure in parser production.";
+		JsonParser parser = new JsonParser(tokens);
+		parser.setTokenFactory(factory);
+		parser.removeErrorListeners(); // remove ConsoleErrorListener
+		parser.addErrorListener(new JsonErrorListener());
+		// parser.setErrorHandler(new JsonParserErrorStrategy());
+		return parser.json();
+
+	}
+
+	private JsonPhase01 processPhase01(JsonContext tree, ParseTreeWalker walker) {
+		lastError = "Failure in parse phase 1.";
+		JsonPhase01 phase01 = new JsonPhase01(state, processor);
+		phase01.collectComments(true);
+		walker.walk(phase01, tree);
+		return phase01;
+	}
+
+	private JsonPhase02 processPhase02(JsonContext tree, ParseTreeWalker walker, JsonPhase01 phase01) {
+		lastError = "Failure in parse phase 2.";
+		JsonPhase02 phase02 = new JsonPhase02(phase01, processor);
+		walker.walk(phase02, tree);
+		if (!JsonPhase02.statusResolved) {
+			Log.warn(this, "Failure to resolve source description in phase2");
+		}
+		return phase02;
+	}
+
+	private JsonPhase03 processPhase03(JsonContext tree, ParseTreeWalker walker, JsonPhase02 phase02) {
+		lastError = "Failure in parse phase 3.";
+		JsonPhase03 phase03 = new JsonPhase03(phase02, processor);
+		walker.walk(phase03, tree);
+		return phase03;
 	}
 }
 
