760e2799e67c9e8cf176d8533b4d961c3b1e76a6
GRosenberg <gbrosenberg85@gmail.com>
2014-12-06 19:10:26 -0800

    Cleanup

--- GenProject.bat
+++ GenProject.bat
@@ -4,13 +4,13 @@ rem Generate the project files
 SETLOCAL
 set STARTTIME=%TIME%
 
-set wkspace=D:\DevFiles\Java\WorkSpaces\Main
+set workspace=D:\DevFiles\Java\WorkSpaces\Main
 set projectName=TomlTest
 set packageName=net.certiv.toml
 set grammarName=Toml
 
 rem well-known locations
-set ruleSet=%wkspace%\GenProject\GenProjectRuleSet.json
+set ruleSet=%workspace%\GenProject\GenProjectRuleSet.json
 set projConfigFile=%workspace%\%projectName%\%grammarName%GenConfig.json
 set genprjar=D:\DevFiles\Java\WorkSpaces\Main\GenProject\jars\GenProject-2.0-complete.jar
 set antlrjar=D:\DevFiles\Java\WorkSpaces\Main\GenProject\lib\antlr-4.4-complete.jar
@@ -19,8 +19,8 @@ set javapgm="%javahome%\bin\java"
 
 set CLASSPATH=%genprjar%;%antlrjar%;%CLASSPATH%
 
-cd /d %wkspace%
-%javapgm% net.certiv.antlr.project.gen.GenProject -c -g %grammarName% -n %packageName% -p %wkspace%\%projectName% -r %ruleSet% %projConfigFile% 
+cd /d %workspace%
+%javapgm% net.certiv.antlr.project.gen.GenProject -c -g %grammarName% -n %packageName% -p %workspace%\%projectName% -r %ruleSet% %projConfigFile% 
 
 set ENDTIME=%TIME%
 set /A STARTTIME=(1%STARTTIME:~6,2%-100)*100 + (1%STARTTIME:~9,2%-100)
